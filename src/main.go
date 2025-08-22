1d9bfbf5a87a7363202f03d0b6b1e98b1c64a5b2
emst444e <emil.steiner@mailbox.tu-dresden.de>
2025-08-22 17:46:23 +0200

    cahnge pivot_root to chroot because invalid value error

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 	//"syscall"
 )
 
@@ -20,10 +21,27 @@ func main() {
 
 func parent() {
 	// '...' is go's spread operator to give elements of a slice seperately in a variadic function
+
+	//	{ //pull and extract an image from dockerhub
+	//		cmd := exec.Command("docker", fmt.Sprintf("export $(docker create %s) -o rootfs.tar.gz", os.Args[2]))
+	//		cmd.Stdin = os.Stdin
+	//		cmd.Stdout = os.Stdout
+	//		cmd.Stderr = os.Stderr
+	//
+	//		if err := cmd.Run(); err != nil {
+	//			fmt.Println("Error", err)
+	//			os.Exit(1)
+	//		}
+	//	}
+
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	}
 
 	if err := cmd.Run(); err != nil {
 		fmt.Println("Error", err)
@@ -32,6 +50,15 @@ func parent() {
 }
 
 func child() {
+
+	//must(syscall.Mount("rootfs", "rootfs", "tmpfs", syscall.MS_BIND|syscall.MS_REC, ""))
+	//must(os.MkdirAll("rootfs/oldrootfs", 0700))
+	//must(syscall.PivotRoot("./rootfs", "./rootfs/oldrootfs"))
+	must(syscall.Chroot("rootfs"))
+	must(os.Chdir("/"))
+
+	must(syscall.Sethostname([]byte("Container")))
+
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -40,5 +67,12 @@ func child() {
 	if err := cmd.Run(); err != nil {
 		fmt.Println("Error", err)
 		os.Exit(1)
+	}
+
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
